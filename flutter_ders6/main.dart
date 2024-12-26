7f36419cddf569c99b8812eaa72134b154f46eba
bekirturgut <bekir.hapoglu@gmail.com>
2024-12-26 14:28:14 +0300

    gereksiz commit

--- flutter_ders6/main.dart
+++ flutter_ders6/main.dart
@@ -1,10 +1,4 @@
-import 'package:ders6/data/local_storage.dart';
-import 'package:ders6/models/task_model.dart';
-import 'package:ders6/pages/home_page.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get_it/get_it.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+
 
 final locator = GetIt.instance;
 
