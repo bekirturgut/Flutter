9fc81acec54c0fd01c9f0c4bcdc3898dde5a9b83
bekirturgut <bekir.hapoglu@gmail.com>
2025-01-15 11:22:29 +0300

    firebase de hesap açma silme çıkış ve giriş işlemleri öğrenildi

--- firebase_baslangic/lib/main.dart
+++ firebase_baslangic/lib/main.dart
@@ -9,6 +9,7 @@ void main() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+    print("Firebase bağlandiiiii");
     runApp(MyApp());
   } catch (e) {
     print("Firebase başlatma hatası VAAAAAARRRRRR: $e");
@@ -26,17 +27,27 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   
   late FirebaseAuth auth;   // firebase giriş kullanımı için
-  final String _Mail = "turgut.hapoglu@gmail.com";
-  final String _sifre = "bekirbekir";
+  final String _Mail = "bekir.turgut.hapoglu@gmail.com";
+  final String _sifre = "bekir.turgut.hapoglu@gmail.com";
   @override
   void initState() {
-    auth = FirebaseAuth.instance;;
+    auth = FirebaseAuth.instance;
     super.initState();
+
+  auth.authStateChanges()
+  .listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out! ,  AÇIK HESAP YOK ULAAAAAN');
+    } else {
+      print('User is signed in!  , AÇIK HESAP VAR ULAAAAAAN ${user.email} ve ${user.emailVerified}');
+    }
+  });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -52,7 +63,16 @@ class _MyAppState extends State<MyApp> {
         children: [
           ElevatedButton(onPressed: () {
             createUserEmailAndPassword();
-          }, child: Text("Email/Şifre Giriş"))
+          }, child: Text("Email/Şifre Kayıt")),
+          ElevatedButton(onPressed: () {
+            LoginUserEmailAndPassword();
+          }, child: Text("Email/Şifre Giriş")),
+          ElevatedButton(onPressed: () {
+            SignOutUser();
+          }, child: Text("Email/Şifre Çıkış")),
+          ElevatedButton(onPressed: () {
+            DeleteUser();
+          }, child: Text("Email/Şifre Sil"))
         ],
       ),
     ),
@@ -60,8 +80,50 @@ class _MyAppState extends State<MyApp> {
 );
 }
   void createUserEmailAndPassword() async{
-    var _UserCredential = (await auth.createUserWithEmailAndPassword(email: _Mail, password: _sifre));
+    try{
+      var UserCredential = (await auth.createUserWithEmailAndPassword(email: _Mail, password: _sifre)); // firebase de mail ve şifre parametrelernini kullanarak hesap kayıt eder.
+      var _myUser = UserCredential.user;
 
-    print(_UserCredential.toString());
+      await _myUser!.sendEmailVerification();
+      if(_myUser!.emailVerified){
+        print("GMAİLE MESAJ GÖNDERİLDİİİİİİİİİİİİİİ");
+      }else{
+        print("EMAİL ONAYLI GEÇEBİLİRSİİİN REİİİİİS");
+      }
+
+      print(UserCredential.toString());
+    }catch(e)
+    {
+      print("\n\nHata alındı\n\n$e");
+    }
+  }
+void LoginUserEmailAndPassword() async{
+  try{
+      auth.signInWithEmailAndPassword(email: _Mail, password: _sifre); // firebase hesap kontrolü yapar ev girşi gerçekleştirir.
+      print("GİRİŞ BAŞARILI ŞEKİLDE GERÇEKLEŞTİ...");
+    }catch(e)
+    {
+      print("\n\nHata alındı\n\n$e");
+    }
+}
+
+void SignOutUser() async{
+  try{
+      await auth.signOut(); // firebase hesap çıkışı yapar..
+      print("ÇIKIŞ BAŞARILI ŞEKİLDE GERÇEKLEŞTİ...");
+    }catch(e)
+    {
+      print("\n\nHata alındı\n\n$e");
+    }
+}
+
+  void DeleteUser() async{
+    try{
+      await auth.currentUser!.delete(); // oluan hesabı siler.
+      print("HESAP SİLİNDİİİİ");
+    }catch(e){
+       print("\n\nHata alındı\n\n$e");
+    }
   }
 }
+
