2e1f88b0aee3e14fe3ab1b54e999607c6dda1056
bekirturgut <bekir.hapoglu@gmail.com>
2025-02-04 19:15:42 +0300

    düzenleme yapıldı

--- Dart Bolum1/HelloWorld.dart
+++
@@ -1,4 +0,0 @@
-void main(List<String> args) {
-  print("Hello Wolrd");
-  print("selam millet batuyla git kurduk.");
-}
---
+++ Dart Bölümleri/Dart Bolum1/HelloWorld.dart
@@ -0,0 +1,4 @@
+void main(List<String> args) {
+  print("Hello Wolrd");
+  print("selam millet batuyla git kurduk.");
+}
